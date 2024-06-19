63956ebe3d30bdd6b31c5800a53be887db5b8f71
Ehor. <ehor.developer@gmail.com>
2024-06-19 16:18:31 +0900

    SSR modeを有効化

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     remix({
-      ssr: false,
+      ssr: true,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
