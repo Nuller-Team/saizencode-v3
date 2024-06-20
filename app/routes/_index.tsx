e4a189f2812f1a1c17e79f26b4ec7f2e7c180c21
Ehor. <123355005+Ehor-developer@users.noreply.github.com>
2024-06-20 13:10:38 +0900

    fix コードの修正

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,27 +13,28 @@ const API_ENDPOINT = 'api/menu';
 
 export default function Index() {
   const [menu, set_menu] = useState<Category[]>([]);
-
   const [query, set_query] = useState('');
   const [limit, setLimit] = useState(10);
   const [selected_category, setselected_category] = useState<Category | null>(null);
-  const [selected_menuitem, setselected_menuitem] = useState<MenuItem | null>(null);
-  const openModal = (category: Category, menuItem: MenuItem) => {
+  const [selected_menuitem, setselected_menu_item] = useState<MenuItem | null>(null);
+  const [loading, setLoading] = useState(true);
+  const openModal = (category: Category, menu_item: MenuItem) => {
     setselected_category(category);
-    setselected_menuitem(menuItem);
+    setselected_menu_item(menu_item);
   };
 
   const closeModal = () => {
     setselected_category(null);
-    setselected_menuitem(null);
+    setselected_menu_item(null);
   };
 
 
 
   useEffect(() => {
     async function fetch_menu() {
+      setLoading(true);
       try {
-        const response = await fetch(`${API_ENDPOINT}?query=${query}&limit=${limit}`);
+        const response = await fetch(`${API_ENDPOINT}?query=${query}`);
         const data = await response.json();
         if (Array.isArray(data)) {
           set_menu(data);
@@ -43,10 +44,11 @@ export default function Index() {
       } catch (error) {
         console.error('Error:', error);
       }
+      setLoading(false);
     }
 
     fetch_menu();
-  }, [query, limit]);
+  }, [query]);
 
 
   return (
@@ -54,7 +56,7 @@ export default function Index() {
       <div className="bg-white text-white p-4 mb-4 rounded-md border-b">
         <div className="max-w-6xl mx-auto w-full">
           <div className="mx-4">
-          <div><img src="logo.webp" className="w-60"></img></div>
+            <div><img src="logo.webp" className="w-60"></img></div>
           </div>
         </div>
       </div>
@@ -74,40 +76,65 @@ export default function Index() {
             />
           </div>
 
-          <div className="pb-40 animate-fadeIn">
-            {menu.map((category) => (
-              <div key={`${category.genre}-${category.reading}`}>
-                <h4 className="text-2xl font-bold sticky top-[84px] bg-white z-[90] px-4 py-1 text-emerald-500 border-l-4 border-emerald-500 md:mx-6 mx-2">
-                  {category.reading}
-                </h4>
-                {category.items.map((menuItem, index) => (
-                  <div className="md:mx-12 mt-4 mx-4" key={index}>
-                    <div
-                      className="flex justify-between items-center bg-white p-4 mb-4 border-2 font-bold rounded-2xl hover:scale-[99%] hover:bg-emerald-50 hover:border-emerald-500 cursor-pointer transition-all z-[1]"
-                      onClick={() => openModal(category, menuItem)}
-                    >
-                      <div className="flex items-center">
-                        <div className="flex justify-center items-center text-white font-bold text-3xl mr-4">
-                          {category.emoji}
-                        </div>
-                        <div className="w-40 md:w-80">
-                          <p className="text-gray-400 text-sm font-bold">{menuItem.id}</p>
-                          <span className="text-lg font-bold text-neutral-600 block truncate">{menuItem.name}</span>
-                        </div>
-                      </div>
-                      <div className="flex items-center text-neutral-600 min-w-[80px] justify-end">
-                        <div>
-                          <span className="text-xl font-bold">{menuItem.tax_price}</span>
-                          <span className="text-sm mx-0.5 my-auto">円</span>
-                        </div>
-                      </div>
+          {loading ? (
+            <div className="pb-40">
+              <div className="animate-pulse">
+                {[...Array(3)].map((_, i) => (
+                  <div key={i}>
+                    <div className="text-2xl font-bold sticky top-[84px] bg-white z-[90] px-4 py-1 text-gray-300 border-l-4 border-gray-300 md:mx-6 mx-2 h-8 w-1/2">
+                      <div className="flex items-center text-gray-300 min-w-[80px] justify-end bg-gray-300 rounded-full h-8 w-20"></div>
                     </div>
+                    {[...Array(5)].map((_, j) => (
+                      <div key={j} className="flex justify-between items-center bg-white p-4 mb-4 border-2 rounded-2xl mt-4 md:mx-12 mx-4">
+                        <div className="flex items-center">
+                          <div className="flex justify-center items-center text-gray-300 font-bold text-3xl mr-4 bg-gray-300 rounded-full w-12 h-12"></div>
+                          <div className="w-40 md:w-80">
+                            <p className="text-gray-300 text-sm font-bold bg-gray-300 rounded h-4 w-1/2"></p>
+                            <span className="text-lg font-bold text-gray-300 block truncate bg-gray-300 rounded h-6 mt-2"></span>
+                          </div>
+                        </div>
+                        <div className="flex items-center text-gray-300 min-w-[80px] justify-end bg-gray-300 rounded-full h-8 w-20"></div>
+                      </div>
+                    ))}
                   </div>
                 ))}
               </div>
-            ))}
-          </div>
-
+            </div>
+          ) : (
+            <div className="pb-40 animate-fadeIn">
+              {menu.map((category) => (
+                <div key={`${category.genre}-${category.reading}`}>
+                  <h4 className="text-2xl font-bold sticky top-[84px] bg-white z-[90] px-4 py-1 text-emerald-500 border-l-4 border-emerald-500 md:mx-6 mx-2">
+                    {category.reading}
+                  </h4>
+                  {category.items.map((menu_item, index) => (
+                    <div className="md:mx-12 mt-4 mx-4" key={index}>
+                      <div
+                        className="flex justify-between items-center bg-white p-4 mb-4 border-2 font-bold rounded-2xl hover:scale-[99%] hover:bg-emerald-50 hover:border-emerald-500 cursor-pointer transition-all z-[1]"
+                        onClick={() => openModal(category, menu_item)}
+                      >
+                        <div className="flex items-center">
+                          <div className="flex justify-center items-center text-white font-bold text-3xl mr-4">
+                            {category.emoji}
+                          </div>
+                          <div className="w-40 md:w-80">
+                            <p className="text-gray-400 text-sm font-bold">{menu_item.id}</p>
+                            <span className="text-lg font-bold text-neutral-600 block truncate">{menu_item.name}</span>
+                          </div>
+                        </div>
+                        <div className="flex items-center text-neutral-600 min-w-[80px] justify-end">
+                          <div>
+                            <span className="text-xl font-bold">{menu_item.tax_price}</span>
+                            <span className="text-sm mx-0.5 my-auto">円</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <Modal
