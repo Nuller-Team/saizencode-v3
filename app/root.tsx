36eeac08cce8a734ea62dda497a13facf477fed1
Ehor. <123355005+Ehor-developer@users.noreply.github.com>
2025-03-15 17:10:01 +0900

    iconのミス

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="favicon.png" type="image/png"/>
+        <link rel="icon" href="icon.png" type="image/png"/>
         <meta name="theme-color" content="white"/>
         <link rel="manifest" href="manifest.json"/>
         <Meta />
