912ec238b0be4acfb55fd9b7e79ad55a2359a1b5
Ehor. <123355005+Ehor-developer@users.noreply.github.com>
2025-03-15 16:27:56 +0900

    Revert "CloudFlareデプロイへ変更"
    This reverts commit e8669836534777694555a4ce0ed7d3e9a5954abf.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { vitePlugin as remix, cloudflareDevProxyVitePlugin } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [cloudflareDevProxyVitePlugin(), remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      ssr: true,
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
